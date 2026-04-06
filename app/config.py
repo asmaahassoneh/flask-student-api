fc4975ac7c424142c4fc4b34390851d58dfb036c
asmaahassoneh <asmaa.hassoneh04@gmail.com>
2026-04-06 23:29:44 +0300

    add a fallback

--- app/config.py
+++ app/config.py
@@ -6,7 +6,7 @@ load_dotenv()
 
 class Config:
     SECRET_KEY = os.getenv("SECRET_KEY", "fallback-secret")
-    SQLALCHEMY_DATABASE_URI = os.getenv("DATABASE_URL")
+    SQLALCHEMY_DATABASE_URI = os.getenv("DATABASE_URL", "sqlite:///students.db")
     SQLALCHEMY_TRACK_MODIFICATIONS = False
 
 
